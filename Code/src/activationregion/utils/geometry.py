17a04c43c8a4c08296a130230e596ab81d1dfe24
Nino Courtecuisse <ncourtecuisse@gmail.com>
2025-01-01 21:52:34 +0100

    Add data for the 10 runs of the experiment

--- Code/src/activationregion/utils/geometry.py
+++ Code/src/activationregion/utils/geometry.py
@@ -9,15 +9,15 @@ def embedding(coordinates, images_plane):
     coordinates = torch.tensor(coordinates, dtype=torch.float32)
     return coordinates[0] * (images_plane[0] - images_plane[1]) + coordinates[1] * (images_plane[0] - images_plane[2]) + images_plane[0]
 
-def compute_line_segment_intersection(p1, p2, line_coeffs, images_plane):
+def embedding1D(coordinates, images_plane):
+    return images_plane[0] + coordinates * (images_plane[1] - images_plane[0])
+
+def compute_line_segment_intersection(p1, p2, line_coeffs, images_plane, embedding=embedding):
     """
     Segment between two points p1, p2: p1 + t * (p2 - p1) with 0 <= t <= 1.
     Line a * x + b * y + c = 0 with line_coeffs: (a, b, c).
     Output: intersection points if it exists, None otherwise.
     """
-    x1, y1 = p1
-    x2, y2 = p2
-    
     p1_emb = embedding(p1[..., np.newaxis], images_plane)
     p2_emb = embedding(p2[..., np.newaxis], images_plane)
 
@@ -31,7 +31,7 @@ def compute_line_segment_intersection(p1, p2, line_coeffs, images_plane):
     t = -(b + w @ p1_emb) / denominator
     t = t.item()
     if 0 < t < 1:              # Check if intersection is within the segment
-        return (x1 + t * (x2 - x1), y1 + t * (y2 - y1))
+        return p1 + t * (p2 - p1)
     return None
 
 def compute_line_region_intersection(convex_hull, line_coeffs, images_plane):
